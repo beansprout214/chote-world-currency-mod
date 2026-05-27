32b42cf3d558a94e1bdd84fc10fdab940c9e42ed
beansprout214 <brightest21@outlook.com>
2026-05-27 03:13:05 -0400

    moved a bunch of methods from CocktailItem.java into CocktailUtil.java, since that's where they should have been to begin with

--- src/main/java/net/hd/cwcurrencymod/item/custom/CocktailItem.java
+++ src/main/java/net/hd/cwcurrencymod/item/custom/CocktailItem.java
@@ -1,39 +1,17 @@
 package net.hd.cwcurrencymod.item.custom;
 
-import com.terraformersmc.modmenu.util.mod.Mod;
 import net.hd.cwcurrencymod.ChoteWorldCurrencyMod;
 import net.hd.cwcurrencymod.component.ModComponents;
-import net.hd.cwcurrencymod.component.custom.CocktailContentsComponent;
 import net.hd.cwcurrencymod.component.custom.UsesRemainingComponent;
-import net.hd.cwcurrencymod.damage_type.ModDamageTypes;
-import net.hd.cwcurrencymod.data.cocktail_types.CocktailRecord;
 import net.hd.cwcurrencymod.item.ModItems;
-import net.hd.cwcurrencymod.util.constants.CocktailTypes;
-import net.hd.cwcurrencymod.util.constants.RegisteredDamageTypes;
-import net.minecraft.component.DataComponentTypes;
+import net.hd.cwcurrencymod.util.CocktailUtil;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsage;
-import net.minecraft.registry.tag.FluidTags;
-import net.minecraft.server.world.ServerWorld;
-import net.minecraft.sound.SoundCategory;
-import net.minecraft.sound.SoundEvents;
 import net.minecraft.stat.Stats;
-import net.minecraft.text.Text;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.Formatting;
-import net.minecraft.util.Hand;
-import net.minecraft.util.hit.BlockHitResult;
-import net.minecraft.util.hit.HitResult;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.RaycastContext;
 import net.minecraft.world.World;
-import net.minecraft.world.event.GameEvent;
-import org.jspecify.annotations.Nullable;
-
-import java.util.List;
 
 public class CocktailItem extends Item {
     public CocktailItem(Settings settings) {
@@ -44,7 +22,7 @@ public class CocktailItem extends Item {
     public ItemStack finishUsing(ItemStack stack, World world, LivingEntity user) {
         var contents = stack.get(ModComponents.COCKTAIL_CONTENTS);
         if (contents != null) {
-            applyCocktailEffect(user, contents.record());
+            CocktailUtil.applyCocktailEffect(user, contents.record());
         }
 
         // if user is in creative mode, dont decrement stack
@@ -77,95 +55,6 @@ public class CocktailItem extends Item {
 
         // otherwise spawn stack in inventory
         return finalStack;
-    }
-
-    public static void applyCocktailEffect(LivingEntity entity, CocktailRecord cocktailRecord) {
-        CocktailTypes type = CocktailTypes.fromId(cocktailRecord.id());
-
-        if(entity.getEntityWorld().isClient()) return;
-
-        ServerWorld serverWorld = (ServerWorld)entity.getEntityWorld();
-        PlayerEntity playerEntity = (PlayerEntity)entity;
-
-        playerEntity.incrementStat(Stats.USED.getOrCreateStat(ModItems.COCKTAIL));
-
-        switch(type) {
-            case WATER:
-                entity.heal(1f);
-                break;
-            case BLAZE_BOURBON:
-                entity.setOnFireForTicks(CocktailRecord.BLAZE_BOURBON.duration());
-                break;
-            case BITE_OF_87:
-                float health = entity.getHealth();
-
-                if (health > 4f) {
-                    entity.damage(serverWorld, ModDamageTypes.getDamageSource(serverWorld, RegisteredDamageTypes.COCKTAIL_BITE_OF_87), health - 1f);
-                    break;
-                }
-                entity.damage(serverWorld, ModDamageTypes.getDamageSource(serverWorld, RegisteredDamageTypes.COCKTAIL_BITE_OF_87), Float.MAX_VALUE);
-                break;
-            case DEATH_SENTENCE:
-                entity.damage(serverWorld, ModDamageTypes.getDamageSource(serverWorld, RegisteredDamageTypes.COCKTAIL_DEATH_SENTENCE), Float.MAX_VALUE);
-                break;
-        }
-    }
-
-    public static ItemStack createCocktail(String key, CocktailRecord record, Formatting color) {
-        ItemStack stack = new ItemStack(ModItems.COCKTAIL);
-
-        stack.set(ModComponents.COCKTAIL_CONTENTS,
-                new CocktailContentsComponent(record));
-
-        stack.set(
-                DataComponentTypes.CUSTOM_NAME,
-                Text.translatable(key)
-                        .formatted(color)
-                        .styled(style -> style.withItalic(false))
-        );
-
-        return stack;
-    }
-
-    public static ItemStack createCocktail(String key, CocktailRecord record) {
-        ItemStack stack = new ItemStack(ModItems.COCKTAIL);
-
-        stack.set(ModComponents.COCKTAIL_CONTENTS,
-                new CocktailContentsComponent(record));
-
-        stack.set(
-                DataComponentTypes.CUSTOM_NAME,
-                Text.translatable(key)
-                        .styled(style -> style.withItalic(false))
-        );
-
-        return stack;
-    }
-
-    @Nullable
-    public static ItemStack getCocktail(CocktailTypes type) {
-        switch(type){
-            case WATER:
-                return CocktailItem.createCocktail("cocktail.chote-world-currency-mod.water", CocktailRecord.WATER);
-
-            case BLAZE_BOURBON:
-                return CocktailItem.createCocktail("cocktail.chote-world-currency-mod.blaze_bourbon", CocktailRecord.BLAZE_BOURBON, Formatting.RED);
-
-            case BITE_OF_87:
-                return CocktailItem.createCocktail("cocktail.chote-world-currency-mod.bite_of_87", CocktailRecord.BITE_OF_87, Formatting.GRAY);
-
-            case DEATH_SENTENCE:
-                return CocktailItem.createCocktail("cocktail.chote-world-currency-mod.death_sentence", CocktailRecord.DEATH_SENTENCE, Formatting.DARK_GRAY);
-
-            case BLAKE_SHAKE:
-                return CocktailItem.createCocktail("cocktail.chote-world-currency-mod.blake_shake", CocktailRecord.BLAKE_SHAKE, Formatting.GREEN);
-
-            case CHOTE_FLOAT:
-                return CocktailItem.createCocktail("cocktail.chote-world-currency-mod.chote_float", CocktailRecord.CHOTE_FLOAT, Formatting.GOLD);
-
-        }
-
-        return null;
     }
 
     private ItemStack fill(ItemStack stack, PlayerEntity player, ItemStack outputStack) {
--- src/main/java/net/hd/cwcurrencymod/util/CocktailUtil.java
+++ src/main/java/net/hd/cwcurrencymod/util/CocktailUtil.java
@@ -1,8 +1,29 @@
 package net.hd.cwcurrencymod.util;
 
 import net.hd.cwcurrencymod.ChoteWorldCurrencyMod;
+import net.hd.cwcurrencymod.component.ModComponents;
 import net.hd.cwcurrencymod.component.custom.CocktailContentsComponent;
+import net.hd.cwcurrencymod.damage_type.ModDamageTypes;
+import net.hd.cwcurrencymod.data.cocktail_types.CocktailRecord;
+import net.hd.cwcurrencymod.item.ModItems;
+import net.hd.cwcurrencymod.item.custom.CocktailItem;
 import net.hd.cwcurrencymod.util.constants.CocktailTypes;
+import net.hd.cwcurrencymod.util.constants.RegisteredDamageTypes;
+import net.minecraft.component.DataComponentTypes;
+import net.minecraft.entity.ItemEntity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.stat.Stats;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+import net.minecraft.util.math.Box;
+import org.jspecify.annotations.Nullable;
+
+import java.util.List;
 
 public class CocktailUtil {
     public static int getColor(CocktailContentsComponent contentsComponent) {
@@ -27,5 +48,128 @@ public class CocktailUtil {
                 break;
         }
         return 0xFFFFFFFF;
+    }
+
+
+    public static void applyCocktailEffect(LivingEntity entity, CocktailRecord cocktailRecord) {
+        CocktailTypes type = CocktailTypes.fromId(cocktailRecord.id());
+
+        if(entity.getEntityWorld().isClient()) return;
+
+        ServerWorld serverWorld = (ServerWorld)entity.getEntityWorld();
+        PlayerEntity playerEntity = (PlayerEntity)entity;
+
+        playerEntity.incrementStat(Stats.USED.getOrCreateStat(ModItems.COCKTAIL));
+
+        switch(type) {
+            case WATER:
+                entity.heal(1f);
+                break;
+            case BLAZE_BOURBON:
+                entity.setOnFireForTicks(CocktailRecord.BLAZE_BOURBON.duration());
+                break;
+            case BITE_OF_87:
+                float health = entity.getHealth();
+
+                if (health > 4f) {
+                    entity.damage(serverWorld, ModDamageTypes.getDamageSource(serverWorld, RegisteredDamageTypes.COCKTAIL_BITE_OF_87), health - 1f);
+                    break;
+                }
+                entity.damage(serverWorld, ModDamageTypes.getDamageSource(serverWorld, RegisteredDamageTypes.COCKTAIL_BITE_OF_87), Float.MAX_VALUE);
+                break;
+            case DEATH_SENTENCE:
+                entity.damage(serverWorld, ModDamageTypes.getDamageSource(serverWorld, RegisteredDamageTypes.COCKTAIL_DEATH_SENTENCE), Float.MAX_VALUE);
+                break;
+        }
+    }
+
+    public static ItemStack createCocktail(String key, CocktailRecord record, Formatting color) {
+        ItemStack stack = new ItemStack(ModItems.COCKTAIL);
+
+        stack.set(ModComponents.COCKTAIL_CONTENTS,
+                new CocktailContentsComponent(record));
+
+        stack.set(
+                DataComponentTypes.CUSTOM_NAME,
+                Text.translatable(key)
+                        .formatted(color)
+                        .styled(style -> style.withItalic(false))
+        );
+
+        return stack;
+    }
+
+    public static ItemStack createCocktail(String key, CocktailRecord record) {
+        ItemStack stack = new ItemStack(ModItems.COCKTAIL);
+
+        stack.set(ModComponents.COCKTAIL_CONTENTS,
+                new CocktailContentsComponent(record));
+
+        stack.set(
+                DataComponentTypes.CUSTOM_NAME,
+                Text.translatable(key)
+                        .styled(style -> style.withItalic(false))
+        );
+
+        return stack;
+    }
+
+    @Nullable
+    public static ItemStack getCocktail(CocktailTypes type) {
+        switch(type){
+            case WATER:
+                return createCocktail("cocktail.chote-world-currency-mod.water", CocktailRecord.WATER);
+
+            case BLAZE_BOURBON:
+                return createCocktail("cocktail.chote-world-currency-mod.blaze_bourbon", CocktailRecord.BLAZE_BOURBON, Formatting.RED);
+
+            case BITE_OF_87:
+                return createCocktail("cocktail.chote-world-currency-mod.bite_of_87", CocktailRecord.BITE_OF_87, Formatting.GRAY);
+
+            case DEATH_SENTENCE:
+                return createCocktail("cocktail.chote-world-currency-mod.death_sentence", CocktailRecord.DEATH_SENTENCE, Formatting.DARK_GRAY);
+
+            case BLAKE_SHAKE:
+                return createCocktail("cocktail.chote-world-currency-mod.blake_shake", CocktailRecord.BLAKE_SHAKE, Formatting.GREEN);
+
+            case CHOTE_FLOAT:
+                return createCocktail("cocktail.chote-world-currency-mod.chote_float", CocktailRecord.CHOTE_FLOAT, Formatting.GOLD);
+
+        }
+
+        return null;
+    }
+
+    private static boolean isCocktailEntityOfType(ItemEntity cocktailEntity, CocktailTypes targetType) {
+        CocktailRecord cocktailRecord = cocktailEntity.getStack().get(ModComponents.COCKTAIL_CONTENTS).record();
+        if (cocktailRecord == null) return false;
+        return CocktailTypes.fromId(cocktailRecord.id()) == targetType;
+    }
+
+    private static boolean isOtherEntity(ItemEntity otherEntity, Item targetItem) {
+        return otherEntity.getStack().getItem().equals(targetItem);
+    }
+
+    private static boolean handleInteraction(ItemEntity cocktailEntity, ItemEntity otherEntity) {
+        if (isOtherEntity(otherEntity, Items.BLAZE_POWDER) && isCocktailEntityOfType(cocktailEntity, CocktailTypes.WATER)) {
+            cocktailEntity.setStack(getCocktail(CocktailTypes.BLAZE_BOURBON));
+            otherEntity.getStack().decrement(1);
+            return true;
+        }
+        return false;
+    }
+
+    public static void interactWithNearby(ItemEntity cocktailEntity) {
+        Box box = cocktailEntity.getBoundingBox().expand(0.75f);
+
+        List<ItemEntity> nearby = cocktailEntity.getEntityWorld().getEntitiesByClass(
+                ItemEntity.class,
+                box,
+                other -> other != cocktailEntity
+        );
+
+        for (ItemEntity otherEntity : nearby) {
+            if (handleInteraction(cocktailEntity, otherEntity)) return;
+        }
     }
 }
