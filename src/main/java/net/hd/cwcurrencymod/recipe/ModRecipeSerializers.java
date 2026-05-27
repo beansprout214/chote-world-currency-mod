bf63b5405a1aaa1ee82b0e6ce68e693e40f58c4f
beansprout214 <brightest21@outlook.com>
2026-05-27 03:13:54 -0400

    goodbye cocktail recipe system

--- src/main/java/net/hd/cwcurrencymod/recipe/ModRecipeSerializers.java
+++ src/main/java/net/hd/cwcurrencymod/recipe/ModRecipeSerializers.java
@@ -1,14 +1,8 @@
 package net.hd.cwcurrencymod.recipe;
 
 import net.hd.cwcurrencymod.ChoteWorldCurrencyMod;
-import net.minecraft.recipe.RecipeSerializer;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
-import net.minecraft.util.Identifier;
 
 public class ModRecipeSerializers {
-
-    public static final RecipeSerializer<?> COCKTAIL_MIXING = RecipeSerializer.SHAPELESS;
 
     public static void register() {
         ChoteWorldCurrencyMod.LOGGER.info("Registering Recipe Serializers for " + ChoteWorldCurrencyMod.MOD_ID);
