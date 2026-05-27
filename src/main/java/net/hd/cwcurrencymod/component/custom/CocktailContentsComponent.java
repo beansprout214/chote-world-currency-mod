b94aa5ab039d1ab8c198d74d548ec8d890eeccb9
beansprout214 <brightest21@outlook.com>
2026-05-27 03:13:26 -0400

    updated codec to be something i actually sort of understand

--- src/main/java/net/hd/cwcurrencymod/component/custom/CocktailContentsComponent.java
+++ src/main/java/net/hd/cwcurrencymod/component/custom/CocktailContentsComponent.java
@@ -1,13 +1,17 @@
 package net.hd.cwcurrencymod.component.custom;
 
 import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.hd.cwcurrencymod.data.cocktail_types.CocktailRecord;
 
 import java.util.List;
 
 public record CocktailContentsComponent(CocktailRecord record) {
+
     public static final Codec<CocktailContentsComponent> CODEC =
-            CocktailRecord.CODEC.xmap(CocktailContentsComponent::new, CocktailContentsComponent::record);
+            RecordCodecBuilder.create(instance -> instance.group(
+                    CocktailRecord.CODEC.fieldOf("record").forGetter(CocktailContentsComponent::record)
+            ).apply(instance, CocktailContentsComponent::new));
 
     public static final CocktailContentsComponent DEFAULT = new CocktailContentsComponent(CocktailRecord.WATER);
 }
